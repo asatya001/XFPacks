4bb3172a49bc3fb664439856b9d181c6118b4fee
asatya001 <asatya001@gmail.com>
2018-10-25 12:48:35 +0530

    my repos with changes and first commit

---
+++ XFSkiaSharpDemo-master/XFSkiaSharpDemo/XFSkiaSharpDemo/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using Xamarin.Forms;
+
+namespace XFSkiaSharpDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void DrawShapesButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new DrawingShapesPage());
+        }
+
+        private void HandlingTouchInteractionsButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new HandlingTouchPage());
+        }
+
+        private void HandlingImagesButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new HandlingImagesPage());
+        }
+
+        private void RenderingAnimationsButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new RenderingAnimationsPage());
+        }
+    }
+}
